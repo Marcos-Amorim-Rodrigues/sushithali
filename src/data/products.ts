3b422b0b12a0c1b9a256175281b5f033bb1a50cd
Marcos Amorim <marcos.amorim.rodr@gmail.com>
2025-03-19 20:03:50 -0300

    chaging products name

--- src/data/products.ts
+++ src/data/products.ts
@@ -4,126 +4,126 @@ export const products: Product[] = [
     {
         id: 1,
         category: 'sushi',
-        name: 'Sushi 1',
+        name: 'California',
         image: '/images/sushi/sushi1.png',
         price: 49
     },
     {
         id: 2,
         category: 'sushi',
-        name: 'Sushi 2',
+        name: 'Futomaki',
         image: '/images/sushi/sushi2.png',
         price: 49
     },
     {
         id: 3,
         category: 'sushi',
-        name: 'Sushi 3',
+        name: 'Gunkan',
         image: '/images/sushi/sushi3.png',
         price: 49
     },
     {
         id: 4,
         category: 'sushi',
-        name: 'Sushi 4',
+        name: 'Salmon Maki',
         image: '/images/sushi/sushi4.png',
         price: 49
     },
     {
         id: 5,
         category: 'sushi',
-        name: 'Sushi 5',
+        name: 'Salmon Maki',
         image: '/images/sushi/sushi5.png',
         price: 49
     },
     {
         id: 6,
         category: 'sushi',
-        name: 'Sushi 6',
+        name: 'Ebiko Maki',
         image: '/images/sushi/sushi6.png',
         price: 49
     },
     {
         id: 7,
         category: 'sushi',
-        name: 'Sushi 7',
+        name: 'Hossomaki Tekkamaki',
         image: '/images/sushi/sushi7.png',
         price: 49
     },
     {
         id: 8,
         category: 'sushi',
-        name: 'Sushi 8',
+        name: 'Uramaki',
         image: '/images/sushi/sushi8.png',
         price: 49
     },
     {
         id: 9,
         category: 'sushi',
-        name: 'Sushi 9',
+        name: 'Uramaki',
         image: '/images/sushi/sushi9.png',
         price: 49
     },
     {
         id: 21,
         category: 'pack',
-        name: 'Pack 1',
+        name: 'The ship',
         image: '/images/combo/combo1.png',
         price: 81
     },
     {
         id: 22,
         category: 'pack',
-        name: 'Pack 2',
+        name: 'Only necessary',
         image: '/images/combo/combo2.png',
         price: 81
     },
     {
         id: 23,
         category: 'pack',
-        name: 'Pack 3',
+        name: 'To satisfy your hungry',
         image: '/images/combo/combo3.png',
         price: 81
     },
     {
         id: 24,
         category: 'pack',
-        name: 'Pack 4',
+        name: 'Lovable',
         image: '/images/combo/combo4.png',
         price: 81
     },
     {
         id: 25,
         category: 'pack',
-        name: 'Pack 5',
+        name: 'Bigger ship',
         image: '/images/combo/combo5.png',
         price: 81
     },
     {
         id: 26,
         category: 'pack',
-        name: 'Pack 6',
+        name: 'Hot Rolls',
         image: '/images/combo/combo6.png',
         price: 81
     },
     {
         id: 27,
         category: 'pack',
-        name: 'Pack 7',
+        name: 'All you need',
         image: '/images/combo/combo7.png',
         price: 81
     },
     {
         id: 28,
         category: 'pack',
-        name: 'Pack 8',
+        name: 'Completed',
         image: '/images/combo/combo8.png',
         price: 81
     },
     {
         id: 29,
         category: 'pack',
-        name: 'Pack 9',
+        name: 'Fck*g great',
         image: '/images/combo/combo9.png',
         price: 81
     },
